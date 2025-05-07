004de3029d2acfddd29aa198ef8ada148ab3a2a6
lasha <maisuradze.llasha@gmail.com>
2025-05-07 19:37:02 +0400

    worked onm header and home style

--- AngularProject/src/app/app.routes.ts
+++ AngularProject/src/app/app.routes.ts
@@ -5,12 +5,16 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ErrorComponent } from './error/error.component';
 import { TrainIdComponent } from './train-id/train-id.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProfileComponent } from './profile/profile.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: "home", pathMatch : "full"},
     {path: 'home', component : HomeComponent},
     {path: 'trains/:id', component : TrainIdComponent},
     {path: 'about', component : AboutComponent},
+    {path: 'contact', component : ContactComponent},
+    {path: 'profile', component : ProfileComponent},
     {path: 'header', component : HeaderComponent},
     {path: 'footer', component : FooterComponent},
     {path: '**', component : ErrorComponent},
