726ecbd071c69ea8892e79f2e2259b19bbfe43d7
lasha <maisuradze.llasha@gmail.com>
2025-05-26 10:13:02 +0400

    profile better now

--- AngularProject/src/app/client-profile/client-profile.component.ts
+++ AngularProject/src/app/client-profile/client-profile.component.ts
@@ -16,18 +16,18 @@ export class ClientProfileComponent {
   constructor(private auth : SignalService) { }
 
   name: string = 'თქვენ არ ხართ დარეგისტრირებული';
-  mail: string = 'თქვენ არ ხართ დარეგისტრირებული';
-  num: string = 'თქვენ არ ხართ დარეგისტრირებული';
-  pass: string = 'თქვენ არ ხართ დარეგისტრირებული';
+  mail: string = '';
+  num: string = '';
+  pass: string = '';
 
   ngOnInit() {
     try {
       const userData = JSON.parse(localStorage.getItem("userObj") || "[]");
       if (userData.length > 0) {
-        this.name = userData[0].name || this.name;
-        this.mail = userData[0].mail || this.mail;
-        this.pass = userData[0].pass || this.pass;
-        this.num  = userData[0].num  || this.num;
+        this.name = 'სახელი: ' + userData[0].name || this.name;
+        this.mail = 'მეილი: ' + userData[0].mail || this.mail;
+        this.pass = 'პაროლი: ' + userData[0].pass || this.pass;
+        this.num  = 'ტელეფონი: ' + userData[0].num  || this.num;
       }
     } catch (e) {
       console.error("Error parsing user data:", e);
@@ -38,9 +38,6 @@ export class ClientProfileComponent {
     localStorage.removeItem('userObj');
     this.auth.deAuth();
     this.name = 'თქვენ არ ხართ დარეგისტრირებული';
-    this.mail = 'თქვენ არ ხართ დარეგისტრირებული';
-    this.num = 'თქვენ არ ხართ დარეგისტრირებული';
-    this.pass = 'თქვენ არ ხართ დარეგისტრირებული';
   }
 
 }
